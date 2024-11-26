972f19217a7bae3b02d3dcbaef6a6e3d459848c6
Julian7Castillo <oscarcastillo212002@gmail.com>
2024-11-26 15:02:49 -0500

    creacion de ventanas de nosotros contracto servicios y login

--- Cafe/urls.py
+++ Cafe/urls.py
@@ -7,6 +7,11 @@ from .views import *
 
 urlpatterns = [
     path('', index,name='index' ),
+    path('servicios/', servicios,name='servicios' ),
+    path('nosotros/', nosotros,name='nosotros' ),
+    path('contactanos/', contactanos,name='contactanos' ),
+    path('login/', Login,name='login' ),
+    path('inicio/', inicio,name='inicio' ),
 ]
 
 
--- Cafe/views.py
+++ Cafe/views.py
@@ -10,3 +10,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def Login(request):
+    return render(request, 'login.html')
+
+def servicios(request):
+    return render(request, 'servicios.html')
+
+def nosotros(request):
+    return render(request, 'nosotros.html')
+
+def contactanos(request):
+    return render(request, 'contactanos.html')
+
+def inicio(request):
+    return render(request, 'inicio.html')
